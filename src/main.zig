deee9aac0c9b3d8f09ed599bcb0b3db38c7c473c
Cédric Creusot <cedric.creusot@gmail.com>
2025-03-21 15:52:54 +0100

    Rework on the is_valid_labyrinth_algorithm

--- src/main.zig
+++ src/main.zig
@@ -27,21 +27,20 @@ pub fn load_labyrinth(allocator: std.mem.Allocator, input: []const u8) std.Array
     return lines;
 }
 
-pub fn is_labyrinth_valid(labyrinth: std.ArrayList([]const u8)) bool {
-    var counter: i32 = 0;
+pub fn is_labyrinth_valid(labyrinth: std.ArrayList([]const u8)) !?[]const Position {
     for (labyrinth.items, 0..) |line, y| {
         for (line, 0..) |cell, x| {
             if ((y == 0 or x == 0 or y == labyrinth.items.len - 1 or x == line.len - 1) and cell == ' ') {
                 // TODO : Create array list here and add first point to start the exploration...
-                if (explore_labyrinth(labyrinth, y, x)) {
-                    std.debug.print("y: {}, x: {}\n", .{ y, x });
-                    counter += 1;
-                    break; // found an exit
-                }
+                var path = std.ArrayList(Position).init(std.heap.page_allocator);
+                defer path.deinit();
+
+                try path.append(Position{ .x = x, .y = y });
+                return explore_labyrinth(labyrinth, path);
             }
         }
     }
-    return counter >= 2;
+    return null;
 }
 
 // STOP WHEN:
@@ -53,45 +52,68 @@ const Position = struct {
     y: usize,
 };
 
-pub fn explore_labyrinth(labyrinth: std.ArrayList([]const u8), path: std.ArrayList(Position)) bool {
+pub fn explore_labyrinth(labyrinth: std.ArrayList([]const u8), path: std.ArrayList(Position)) ?[]const Position {
     // return .{
     //  .valid = true/false
     //  .path = {{x,y}, ...}
     // }
     // recursion
     const last_pos = path.getLast();
-    const x, const y = last_pos;
+    const x = last_pos.x;
+    const y = last_pos.y;
 
     // STOP WALL
     if (labyrinth.items[y][x] != ' ') {
-        return false;
+        return null;
     }
 
     // STOP EDGE
-    if (path.len > 1 and (x == 0 or x == labyrinth.items[y].len - 1 or y == 0 or y == labyrinth.items.len - 1)) {
-        return true;
+    if (path.items.len > 1 and (x == 0 or x == labyrinth.items[y].len - 1 or y == 0 or y == labyrinth.items.len - 1)) {
+        return path.items;
     }
 
     // EXPLORATION
-    const north: Position = if (y > 0) .{ .x = x, .y = y - 1 } else null;
+    const north: ?Position = if (y > 0) .{ .x = x, .y = y - 1 } else null;
     const south: ?Position = if (y + 1 < labyrinth.items.len) .{ .x = x, .y = y + 1 } else null;
     const west: ?Position = if (x > 0) .{ .x = x - 1, .y = y } else null;
     const east: ?Position = if (x + 1 < labyrinth.items[y].len) .{ .x = x + 1, .y = y } else null;
 
-    return ((north != null and path.contains(north) == false and explore_labyrinth(labyrinth, path.clone().append(north)) == true) or (south != null and path.contains(south) == false and explore_labyrinth(labyrinth, path.clone().append(south)) == true) or (west != null and path.contains(west) == false and explore_labyrinth(labyrinth, path.clone().append(west)) == true) or (east != null and path.contains(east) == false and explore_labyrinth(labyrinth, path.clone().append(east)) == true));
+    if (search_at(labyrinth, path, north)) |new_path| {
+        return new_path;
+    }
+    if (search_at(labyrinth, path, south)) |new_path| {
+        return new_path;
+    }
+    if (search_at(labyrinth, path, west)) |new_path| {
+        return new_path;
+    }
+    if (search_at(labyrinth, path, east)) |new_path| {
+        return new_path;
+    }
+    return null;
+}
 
-    // if (x > 0 and labyrinth.items[y][x - 1] == ' ') { // W
-    //     return true;
-    // } else if (y > 0 and labyrinth.items[y - 1][x] == ' ') { // N
-    //     return true;
-    // } else if (x + 1 < labyrinth.items[y].len and labyrinth.items[y][x + 1] == ' ') { //E
-    //     return true;
-    // } else if (y + 1 < labyrinth.items.len and labyrinth.items[y + 1][x] == ' ') { // S
-    //     return true;
-    // }
+pub fn search_at(labyrinth: std.ArrayList([]const u8), path: std.ArrayList(Position), opt_position: ?Position) ?[]const Position {
+    if (opt_position) |position| {
+        if (in_path(path.items, position)) {
+            return null;
+        }
+        var cloned = path.clone() catch {
+            return null;
+        };
+        cloned.append(position) catch unreachable;
+        return explore_labyrinth(labyrinth, cloned);
+    }
+    return null;
+}
 
-    // fin de recursion
-    // return false;
+pub fn in_path(path: []const Position, to_find: Position) bool {
+    for (path) |pos| {
+        if (pos.x == to_find.x and pos.y == to_find.y) {
+            return true;
+        }
+    }
+    return false;
 }
 
 test "Load Labyrinth" {
@@ -122,7 +144,7 @@ test "Is Labyrinth not valid" {
     );
     defer labyrinth.deinit();
 
-    try std.testing.expectEqual(false, is_labyrinth_valid(labyrinth));
+    try std.testing.expectEqual(null, try is_labyrinth_valid(labyrinth));
 }
 
 test "Is Labyrinth valid vertical line representation" {
@@ -133,7 +155,7 @@ test "Is Labyrinth valid vertical line representation" {
     );
     defer labyrinth.deinit();
 
-    try std.testing.expectEqual(true, is_labyrinth_valid(labyrinth));
+    try std.testing.expectEqual(true, try is_labyrinth_valid(labyrinth) != null);
 }
 
 test "Is Labyrinth valid horintal line representation" {
@@ -144,7 +166,7 @@ test "Is Labyrinth valid horintal line representation" {
     );
     defer labyrinth.deinit();
 
-    try std.testing.expectEqual(true, is_labyrinth_valid(labyrinth));
+    try std.testing.expectEqual(true, try is_labyrinth_valid(labyrinth) != null);
 }
 
 test "Is Labyrinth invalid with empty corner" {
@@ -155,7 +177,7 @@ test "Is Labyrinth invalid with empty corner" {
     );
     defer labyrinth.deinit();
 
-    try std.testing.expectEqual(false, is_labyrinth_valid(labyrinth));
+    try std.testing.expectEqual(null, try is_labyrinth_valid(labyrinth));
 }
 
 test "U Labyrinth is invalid" {
@@ -166,7 +188,7 @@ test "U Labyrinth is invalid" {
     );
     defer labyrinth.deinit();
 
-    try std.testing.expectEqual(false, is_labyrinth_valid(labyrinth));
+    try std.testing.expectEqual(null, try is_labyrinth_valid(labyrinth));
 }
 
 test "10x10 Labyrinth with no exit" {
@@ -185,7 +207,26 @@ test "10x10 Labyrinth with no exit" {
 
     defer labyrinth.deinit();
 
-    try std.testing.expectEqual(false, is_labyrinth_valid(labyrinth));
+    try std.testing.expectEqual(null, try is_labyrinth_valid(labyrinth));
+}
+
+test "10x10 Labyrinth with exit" {
+    const labyrinth = load_labyrinth(std.testing.allocator,
+        \\x xxxxxxxx
+        \\x    x x x
+        \\x x    x x
+        \\x  xx  x x
+        \\x xxxxxxxx
+        \\x        x
+        \\x    xxxxx
+        \\xxxx x   x
+        \\x      x x
+        \\xxxxxxxx x
+    );
+
+    defer labyrinth.deinit();
+
+    try std.testing.expectEqual(true, try is_labyrinth_valid(labyrinth) != null);
 }
 
 // TODO : Print test name
